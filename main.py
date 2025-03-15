ca9f243f6ee3d153a2345c4370bcee93a40909aa
seveloz <sjvlmx@gmail.com>
2025-03-15 12:21:41 -0600

    add split

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
 
         screen.fill("black")
